b8bbef2f441911af33991906708844248b41668d
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2022-06-06 15:56:21 -0300

    swagger annotations

--- src/main/java/com/ufcg/university/controllers/ProfessorController.java
+++ src/main/java/com/ufcg/university/controllers/ProfessorController.java
@@ -17,7 +17,16 @@ import com.ufcg.university.entities.Professor;
 import com.ufcg.university.services.ProfessorService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.AuthorizationScope;
+import io.swagger.annotations.ResponseHeader;
 
 @RestController
 @RequestMapping(value = "/professor")
@@ -28,19 +37,46 @@ public class ProfessorController {
 	private ProfessorService professorService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	@ApiOperation(value = "List All Professors")
+	@ApiOperation(value = "List All Professors",
+				  notes = "Multiple Professors can be provided in a list",
+				  response = ProfessorDTO.class,
+				  responseContainer = "List"
+	)
+	
+	@ApiModelProperty(allowableValues = "list of professors")
 	public ResponseEntity<List<ProfessorDTO>> getAllProfessors() {
 		return new ResponseEntity<>(this.professorService.listProfessors(), HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	@ApiOperation(value = "Create a Professor")
+	@ApiOperation(value = "Create a Professor", 
+		authorizations = {
+			    @Authorization(
+				        value = "universityoauth", // scheme 
+					    scopes = {
+					        @AuthorizationScope(
+					        		scope = "add:professor", 
+					        		description = "Allows adding of professors")
+						     }
+				)
+		},
+		responseHeaders = {
+				@ResponseHeader(description = "It's return a Status Code")
+		}
+	)
+	
 	public ResponseEntity<Professor> createProfessor(@RequestBody ProfessorDTO professorDTO) {
 		return new ResponseEntity<>(this.professorService.createProfessor(professorDTO), HttpStatus.CREATED);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ApiOperation(value = "Get Professor By Id")
+	@ApiResponses(
+			value = {
+					@ApiResponse(code = 400, message = "Invalid ID"),
+					@ApiResponse(code = 404, message = "Professor Not Found") 
+			}
+	)
 	public ResponseEntity<?> getProfessorById(@PathVariable("id") Long id) {
 		try {
 			return new ResponseEntity<>(this.professorService.getProfessorById(id), HttpStatus.OK);
@@ -61,7 +97,14 @@ public class ProfessorController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.PUT)
 	@ApiOperation(value = "Update Professor")
-	public ResponseEntity<?> updateProfessorById(@RequestParam("id") Long id, @RequestBody ProfessorDTO professorDTO) {
+	@ApiImplicitParams({
+	    @ApiImplicitParam(name = "name", value = "Professor's name", required = true, dataType = "string", paramType = "body"),
+	    @ApiImplicitParam(name = "password", value = "Professor's password", required = true, dataType = "string", paramType = "body"),
+	    @ApiImplicitParam(name = "serviceTime", value = "Professor's service time", required = true, dataType = "int", paramType = "body"),
+	    @ApiImplicitParam(name = "discipline", value = "Professor's discipline", required = true, dataType = "string", paramType = "body")
+	})
+	public ResponseEntity<?> updateProfessorById(@RequestParam("id") Long id, 
+												 @ApiParam(value = "Updated professor object", required = true) ProfessorDTO professorDTO) {
 		try {
 			return new ResponseEntity<>(this.professorService.updateProfessor(id, professorDTO), HttpStatus.OK);
 		} catch (Exception e) {
