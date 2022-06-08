03e7649317fb504049d450e521161a32e647d6a7
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2022-06-08 09:30:35 -0300

    fixing swagger jwt token

--- src/main/java/com/ufcg/university/settings/SwaggerConfig.java
+++ src/main/java/com/ufcg/university/settings/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.ufcg.university.settings;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +12,10 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -23,10 +27,22 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	    return new ApiKey("JWT", "Authorization", "header"); 
 	}
 	
+	private SecurityContext securityContext() { 
+	    return SecurityContext.builder().securityReferences(defaultAuth()).build(); 
+	}
+	
+	private List<SecurityReference> defaultAuth() { 
+	    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
+	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1]; 
+	    authorizationScopes[0] = authorizationScope; 
+	    return Arrays.asList(new SecurityReference("JWT", authorizationScopes)); 
+	}
+	
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.ufcg.university"))
                 .build()
+                .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(metaData());
     }
