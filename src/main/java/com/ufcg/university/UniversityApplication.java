96df2ff4cb845f53a2a105dce122e2b31c500b4d
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2022-06-20 10:28:54 -0300

    updating

--- src/main/java/com/ufcg/university/UniversityApplication.java
+++ src/main/java/com/ufcg/university/UniversityApplication.java
@@ -5,13 +5,10 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ufcg.university.parser.MetricsParser;
-
 @SpringBootApplication
 public class UniversityApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(UniversityApplication.class, args);
-		MetricsParser mp = new MetricsParser();
 	}
 }
