9d01aea8c7524c33fd9479defc2822798a1ab5bf
maely brandão <56039385+mxlysb@users.noreply.github.com>
2022-08-04 10:59:44 -0300

    com erro

--- src/test/java/com/ufcg/university/services/ProfessorServiceTest.java
+++ src/test/java/com/ufcg/university/services/ProfessorServiceTest.java
@@ -2,33 +2,76 @@ package com.ufcg.university.services;
 
 import com.ufcg.university.dto.ProfessorDTO;
 import com.ufcg.university.entities.Professor;
+import com.ufcg.university.mapper.ProfessorMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //@SpringBootConfiguration
 //@SpringBootTest
 //@EnableAutoConfiguration
 public class ProfessorServiceTest {
-    //    @MockBean
-    ProfessorService profService = new ProfessorService();
+    @Autowired
+    private ProfessorService profService;
+
+    @Autowired
+    private ProfessorMapper professorMapper;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     private ProfessorDTO prof;
 
-    public static Stream<Arguments> professorCases(){
-        return Stream.of(
-                Arguments.of(new ProfessorDTO("JoÃ£o","1234",1,"ProgramaÃ§ao")),
-                Arguments.of(new ProfessorDTO("Ramon","78910",4,"Banco de Dados"))
-        );
-
+    @BeforeEach
+    public void beforeTests(){
+        profService.createProfessor(new ProfessorDTO("Ennyo","1234",1,"ProgramaÃ§ao"));
+        profService.createProfessor(new ProfessorDTO("Ramon","78910",4,"Banco de Dados"));
     }
     @Test
     public void createdProfessorTest(){
-        System.out.println(" sdfas");
-        prof = new ProfessorDTO("Maria","12345",3,"BZ");
-        Professor prof2 = new Professor("Maria","12345",3,"BZ");
-        assertEquals(prof2,profService.createProfessor(prof));
+        prof = new ProfessorDTO("Maria","12345",3,"EDA");
+        Professor prof2 = new Professor("Maria","12345",3,"EDA");
+        Professor prof3 = profService.createProfessor(prof);
+        assertTrue(bCryptPasswordEncoder.matches(prof2.getPassword(), prof3.getPassword()));
     }
+
+    @Test
+    public void getProfessorTest(){
+        prof = new ProfessorDTO("Maria","12345",3,"EDA");
+        Professor prof2 = profService.createProfessor(prof);
+        try {
+            ProfessorDTO prof3 = profService.getProfessorById(prof2.getId());
+            Professor prof4 = professorMapper.convertFromProfessorDTO(prof3);
+            assertEquals(prof2,prof4);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void listProfessors(){
+        System.out.println("************************************************************");
+        profService.listProfessors();
+        System.out.println("************************************************************");
+    }
+
+    @Test
+    public void updateProfessor(){
+        prof = new ProfessorDTO("Maria","12345",3,"EDA");
+        Professor prof2 = profService.createProfessor(prof);
+        ProfessorDTO prof3 = new ProfessorDTO("Maria", "223344", 3, "EDA");
+        try {
+            ProfessorDTO profUpdated = profService.updateProfessor(prof2.getId(), prof3);
+            assertTrue(bCryptPasswordEncoder.matches(prof3.getPassword(), profUpdated.getPassword()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- src/test/java/com/ufcg/university/services/StudentServiceTest.java
+++ src/test/java/com/ufcg/university/services/StudentServiceTest.java
@@ -1,29 +1,63 @@
 package com.ufcg.university.services;
 
+import com.ufcg.university.dto.ProfessorDTO;
 import com.ufcg.university.dto.StudentDTO;
+import com.ufcg.university.entities.Professor;
 import com.ufcg.university.entities.Student;
+import com.ufcg.university.mapper.ProfessorMapper;
+import com.ufcg.university.mapper.StudentMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StudentServiceTest {
 
-    StudentService studentService = new StudentService();
+    @Autowired
+    private StudentService studentService = new StudentService();
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     private StudentDTO studentDTO;
 
-    public static Stream<Arguments> studentsCases() {
-        return Stream.of (
-                Arguments.of(new StudentDTO("Pedro", "123456", "Programação")),
-                Arguments.of(new StudentDTO("Matheus", "09876", "Banco de Dados")));
-    }
+    @Autowired
+    private StudentMapper studentMapper;
+    
     @Test
     public void createdStudentTest() {
         studentDTO = new StudentDTO("Maria", "12345", "oi");
         Student student2 = new Student("Maria", "12345", "oi");
 
-        assertEquals(student2, studentService.createStudent(studentDTO));
+        Student student = studentService.createStudent(studentDTO);
+        assertTrue(bCryptPasswordEncoder.matches(student2.getPassword(), student.getPassword()));
+    }
+
+    @Test
+    public void getStudentTest(){
+        studentDTO = new StudentDTO("Maria","12345","EDA");
+        Student student = studentService.createStudent(studentDTO);
+        try {
+            StudentDTO student2 = studentService.getStudentById(student.getId());
+            Student student3 = studentMapper.convertFromStudentDTO(student2);
+            assertEquals(student,student3);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void updateStudent(){
+        studentDTO = new StudentDTO("Maria","12345","EDA");
+        Student student = studentService.createStudent(studentDTO);
+        StudentDTO studentDTO2 = new StudentDTO("Maria", "223344", "EDA");
+        try {
+            StudentDTO studentUpdated = studentService.updateStudent(student.getId(), studentDTO2);
+            assertTrue(bCryptPasswordEncoder.matches(studentDTO2.getPassword(), studentUpdated.getPassword()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
